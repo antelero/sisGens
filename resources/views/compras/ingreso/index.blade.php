3318f0552938b9d376aec62f2de15e379a2ae4ab
antelero <lmbrambilla@gmail.com>
2018-12-20 18:51:57 -0300

    Update index.blade.php

--- resources/views/compras/ingreso/index.blade.php
+++ resources/views/compras/ingreso/index.blade.php
@@ -26,7 +26,7 @@
 					
 					<td>{{ $ing->fecha_hora}}</td>
 					<td>{{ $ing->nombre}}</td>
-					<td>{{ $ing->tipo_comprobante.':'.$ing->serie_comprobante.' - '.$ing->num_comprobante}}</td>					
+					<td>{{ $ing->tipo_comprobante.': '.$ing->serie_comprobante.' - '.$ing->num_comprobante}}</td>					
 					<td>{{ $ing->impuesto}}</td>
 					<td>{{ $ing->total}}</td>
 					<td>{{ $ing->estado}}</td>
