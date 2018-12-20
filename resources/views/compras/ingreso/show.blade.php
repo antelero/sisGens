f6b720934fb35fc9963407087a5552304756855a
antelero <lmbrambilla@gmail.com>
2018-12-20 18:28:42 -0300

    1

--- resources/views/compras/ingreso/show.blade.php
+++ resources/views/compras/ingreso/show.blade.php
@@ -35,8 +35,8 @@
             			
            			 </div>
 				</div> 		
-			</div>
-			<div class="row">
+	</div>
+	<div class="row">
 				
 						<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
 							<table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
@@ -81,7 +81,12 @@
 							</table>
 						</div>
 			</div>
-	</div>
+			<div class="col-lg-6 col-md-6 col-xs-12">
+				
+            			<a href="{{ url()->previous() }}" class="btn btn-danger">Volver</a>
+            </div>
+	</div> 
+	
         
             
 
